4a52c75a1a6d7b8891ce9c648e2b95075919359c
ShaikAbdulRazzaaq <shaik.11715485@lpu.in>
2024-02-02 05:07:29 +0530

    refactor(Code): Added some comments in Constants

--- app/src/main/java/com/practice/todo/utils/Constants.kt
+++ app/src/main/java/com/practice/todo/utils/Constants.kt
@@ -1,12 +1,13 @@
 package com.practice.todo.utils
 
 object Constants {
+    //Database
     const val DATABASE_TABLE_NAME = "ToDo_Table"
     const val DATABASE_NAME = "Database_ToDo"
 
+    //Navigation
     const val LIST_SCREEN = "list/{action}"
     const val TASK_SCREEN = "task/{taskId}"
-
     const val LIST_ARGUMENT_KEY = "action"
     const val TASK_ARGUMENT_KEY = "taskId"
 }
